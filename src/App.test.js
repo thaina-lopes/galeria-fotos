8280769e9f92b1d87bfc284c9c743e1bab8ec2bc
Thainá Lopes <thainalopes@gmail.com>
2025-01-08 19:05:26 -0300

    Adiciona testes unitários com cobertura de código

---
+++ src/App.test.js
@@ -0,0 +1,35 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
+import "@testing-library/jest-dom";
+
+test("Se a quantidade das imagens foram renderizadas corretamente", () => {
+  render(<App />);
+
+  const imagens = screen.getAllByRole("img", { hidden: false });
+  expect(imagens).toHaveLength(10);
+});
+
+test("Se a mensagem de nenhuma foto encontrada foi renderizada", () => {
+  render(<App />);
+
+  const input = screen.getByPlaceholderText(/pesquise por palavras-chave/i);
+  fireEvent.change(input, { target: { value: "Girafa" } });
+
+  const imagens = screen.queryAllByRole("img", { hidden: false });
+
+  expect(imagens).toHaveLength(0);
+
+  expect(screen.getByText("Nenhuma foto encontrada")).toBeInTheDocument();
+});
+
+test("Se o campo de busca filtra as fotos corretamente", () => {
+  render(<App />);
+
+  const input = screen.getByPlaceholderText(/pesquise por palavras-chave/i);
+  fireEvent.change(input, { target: { value: "Galinha" } });
+
+  const imagens = screen.getAllByRole("img", { hidden: false });
+
+  expect(imagens).toHaveLength(1);
+  expect(screen.getByAltText("Galinha")).toBeInTheDocument();
+});
